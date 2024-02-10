9225f9315aad40a5ec6f15fc6e921ae4279ca4a4
Babu_ka_blouse <101854714+GauravM004@users.noreply.github.com>
2024-02-09 22:09:06 -0800

    Second Commit

--- 02useEffectHook/src/App.jsx
+++ 02useEffectHook/src/App.jsx
@@ -3,27 +3,27 @@ import { useState, useEffect } from 'react'
 
 
 
-//   const [count, setCounter] = useState(0)
-//   const [data, setData] = useState("Ram")
-//   useEffect(() => {
-//     console.log("Component ")
-//   }, [data])
+  const [count, setCounter] = useState(0)
+  const [data, setData] = useState("Ram")
+  useEffect(() => {
+    console.log("Component ")
+  }, [data])
 
-//   function updateCount() {
-//     setCounter(count + 1)
-//   }
-//   function updateData() {
-//     setData("seeta")
-//   }
-//   
+  function updateCount() {
+    setCounter(count + 1)
+  }
+  function updateData() {
+    setData("seeta")
+  }
+  
       
-//       <h1>Button Clicked {count} times</h1>
-//       <button onClick={updateCount}> Click </button>
-//       <h1>Button Clicked {data}</h1>
-//       <button onClick={updateData}>Update Data</button>
-//    
-//   )
-// }
+      <h1>Button Clicked {count} times</h1>
+      <button onClick={updateCount}> Click </button>
+      <h1>Button Clicked {data}</h1>
+      <button onClick={updateData}>Update Data</button>
+   
+  )
+}
 function App() {
 
   return (
